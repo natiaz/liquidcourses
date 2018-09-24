9036cf972f3536ed24295a758ac8551bd299292b
Natiaz <natalia.diaz@tecnilogica.com>
2018-09-24 16:38:41 +0200

    Setting models

--- app/Course.php
+++ app/Course.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+  const COURSE_OPEN = 'open';
+  const COURSE_CLOSE = 'close';
+
+  protected $fillable = ['name', 'description', 'maximum', 'status'];
+
+  public function isOpen()
+  {
+    return $this->status == Course::COURSE_OPEN;
+  }
 }
--- app/User.php
+++ app/User.php
@@ -7,23 +7,44 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+  use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  const USER_VERIFIED = '1';
+  const USER_NOT_VERIFIED = '0';
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  const USER_ADMIN = 'true';
+  const USER_REGULAR = 'false';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'name', 'email', 'password','verified','verification_token','admin'
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+      'password', 'remember_token', 'verification_token'
+  ];
+
+  public function isVerified()
+  {
+    return $this->verified == User::USER_VERIFIED;
+  }
+
+  public function isAdmin()
+  {
+    return $this->admin == User::USER_ADMIN;
+  }
+
+  public static function generateVerificationToken()
+  {
+    return str_random(40);
+  }
 }
